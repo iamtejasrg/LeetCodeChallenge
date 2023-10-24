75d7a3f375b8cd93d5b505248c3691a88a4f8e35
Tejas Gorde <95203031+iamtejasrg@users.noreply.github.com>
2023-10-24 22:05:06 +0530

    Update main.cpp

--- 7_MultiplyStrings/main.cpp
+++ 7_MultiplyStrings/main.cpp
@@ -1,4 +1,4 @@
-class Solution { 
+class Solution {  
 private:       
     // Calculate the sum of all of the results from multiplyOneDigit. 
     string sumResults(vector<vector<int>>& results) {     
